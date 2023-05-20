a155f4ef2b8e926be7490e547d70a618c8e70bff
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-05-19 20:13:16 -0500

    save note functional

--- Develop/helpers.js
+++ Develop/helpers.js
@@ -15,9 +15,4 @@ const readAndAppend = (content, file) => {
     });
   };
 
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-);
-
-module.exports = { readAndAppend, writeToFile, readFromFile }
+module.exports = { readAndAppend, readFromFile }
